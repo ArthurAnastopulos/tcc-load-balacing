914117d1498c0bff4f0ab7669c6f358fdb4b53a9
Arthur Anastopulos <arthur.santos@khomp.com>
2024-07-31 16:27:54 -0300

    outro teste do script

--- SIPp/pcap_to_influx.py
+++ SIPp/pcap_to_influx.py
@@ -18,7 +18,7 @@ def write_to_influxdb_http(url, db, port, json_body):
         print(f"Error writing data: {response.text}")
 
 def process_csv_to_influx(csv_file, url, db, db_table, port):
-    df = pd.read_csv(csv_file, delimiter=' ')  # Adjust delimiter as needed
+    df = pd.read_csv(csv_file)  # Adjust delimiter as needed
         
     # Print headers for debugging
     print("CSV Headers:", df.columns)
